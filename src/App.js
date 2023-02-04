2e80040e61abbe07eab65002ebe2312cb8d6bd92
Ortega <gabs.ortega2610@gmail.com>
2023-02-04 00:04:49 -0300

    About + Start Skills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
-import Home from './components/home/Home'
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
 
     <main className='main'>
       <Home />
+      <About />
+      <Skills />
     </main>
     </>
   )
