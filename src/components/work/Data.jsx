7998d082bd74f5b4b049bf9fffff64758fb52271
Ortega <gabs.ortega2610@gmail.com>
2023-02-06 01:04:47 -0300

    Start Work - Portfolio

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,32 @@
+import Work1 from "../../assets/img/vnom_work.png";
+import Work2 from "../../assets/img/criptphy_work.png";
+
+export const projectsData = projectsData = [
+    {
+      id: 1,
+      image: Work1,
+      title: "VNOM - Serviços e Pessoas",
+      category: "web",
+    },
+    {
+      id: 2,
+      image: Work2,
+      title: "Criptphy",
+      category: "web",
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'web',
+    },
+    {
+        name: 'app',
+    },
+    {
+        name: 'design',
+    },
+];
